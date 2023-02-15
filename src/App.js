23ff208898613cd91a8276089168888cef28eb8f
Raffi <Raffi@DESKTOP-86NFDVV>
2023-02-15 18:01:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [addingMember,setAddingMember] = useState(false);
+  const [searchID, setSearchID] = useState();
+  const [member,setCurrentMember] = useState({});
+  const [memberFound,setMemberFound] = useState(false);
+  const [newPoints,setNewPoints] = useState();
+
+  function searchMember(event){
+        axios.get("http://localhost:5268/api/Membership/getmember",{params:{memberId:event}}).then(function(response)
+        {
+          console.log(response.data);
+          setCurrentMember(response.data);
+          if(response.data.name != "non found")
+          {
+            setMemberFound(true);
+          }
+        });
+  }
+
+  const changeMemberType = (event) => {
+    console.log(event);
+    axios.post("http://localhost:5268/api/Membership/setmembertype",null,{params:{memberId:member.memberId,membershipType:event}})
+    .catch(function(error)
+    {
+      console.log(error);
+    })
+  }
+
+  const setPoints = (event) =>{
+    if(event.key === "Enter"){
+      axios.post("http://localhost:5268/api/Membership/setpoints",null,{params:{points:newPoints,memberId:member.memberId}})
+      .catch(function(error)
+      {
+          console.log(error);
+      })
+      console.log(member.memberId);
+      searchMember(member.memberId);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {addingMember ?  null :<div id="addmember"> <button>Add member!</button></div>}
+      <div><input type="text" placeholder='Enter members id nummer to search!' onChange={e => setSearchID(e.target.value)} onKeyDown={(e) => searchMember(searchID)}></input></div>
+      <div>Name: {member.name}</div>
+      <div>Points: {member.points}</div>
+      <div>Membership type: {member.memberType}</div>
+      <div>Member id: {member.memberId}</div>
+      <div><button onClick={(type)=>changeMemberType("bronze")}>Bronze</button><button onClick={(type)=>changeMemberType("silver")}>Silver</button><button onClick={(type)=>changeMemberType("gold")}>Gold</button></div>
+      <div><input type="number" onChange={(points) => setNewPoints(points.target.value)} onKeyDown={setPoints} placeholder="Add/remove points"></input></div>
     </div>
   );
 }
